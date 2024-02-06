4835c302415bf0114e332ff163c4f0bdcb139943
goranurukalo <dev.goranurukalo@gmail.com>
2024-02-06 14:17:55 +0100

    Fixed issue with build for browser

--- dist/ondemand-client-browser.js
+++ dist/ondemand-client-browser.js
@@ -4,6 +4,7 @@ var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
@@ -23,6 +24,10 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
   mod
 ));
+var __publicField = (obj, key, value) => {
+  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+  return value;
+};
 
 // node_modules/axios/lib/helpers/bind.js
 var require_bind = __commonJS({
@@ -1070,9 +1075,9 @@ var JSONP = class {
 
 // src/ondemand-client.ts
 var OnDemandClient = class {
-  config;
-  jsonp;
   constructor(builder) {
+    __publicField(this, "config");
+    __publicField(this, "jsonp");
     this.config = {
       baseUrl: "https://ondemand.websol.barchart.com",
       apiKey: null,
